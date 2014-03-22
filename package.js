1b52a0ec42683dbd20be3db51f461a744b4bbd26
Ian Serlin <ian@ianserlin.com>
2014-03-21 17:11:09 -0700

    expose client/server braintree api to client and server

--- package.js
+++ package.js
@@ -2,7 +2,10 @@ Package.describe({
   summary: "Exposes BrainTree's Payment Gateway Interface"
 });
 
+Npm.depends({ braintree: 'v1.13.1' });
+
 Package.on_use(function (api) {
-  api.add_files('braintree.js', ['client','server']);
-  api.export('BrainTree', ['client','server']);
+  api.add_files('server.js', ['server']);
+  api.add_files('client.js', ['client']);
+  api.export('Braintree', ['server']);
 });
